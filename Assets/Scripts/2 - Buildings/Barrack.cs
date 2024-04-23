0bf6d161e8fa1ce6d411bec37f5f9dfc192ebea2
Mikhail000 <mish.gracheff@gmail.com>
2024-04-23 15:40:57 +0700

    23.04.2024

---
+++ Assets/Scripts/2 - Buildings/Barrack.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Barrack : MonoBehaviour, ISelectable
+{
+
+    public bool Selected { get; }
+    public bool Hovering { get; }
+    public void Select()
+    {
+        
+    }
+
+    public void Deselect()
+    {
+        
+    }
+
+    public void HoverEnter()
+    {
+        
+    }
+
+    public void HoverExit()
+    {
+        
+    }
+    
+    private void ShowMenu()
+    {
+        
+    }
+}
